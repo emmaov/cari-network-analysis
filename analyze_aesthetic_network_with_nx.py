b0cb27e23e19872611516b17d6f8f2ac79dc0855
emmaov <102431113+emmaov@users.noreply.github.com>
2022-04-22 15:43:47 -0400

    change how graph is constructed to add name attribute

--- analyze_aesthetic_network_with_nx.py
+++ analyze_aesthetic_network_with_nx.py
@@ -1,42 +1,31 @@
 import json
 import csv
 import networkx as nx
-import matplotlib.pyplot as plt
 
 source_file = "cari_data.json"
 
 G = nx.Graph()
 
-nodelist = []
-edgelist = []
-
-#create the node and edge lists
+#construct the graph by iterating through the json file
 with open(source_file, "r") as json_file:
     data = json.load(json_file)
     for aesthetic_category in data:
-        aesthetic_id = aesthetic_category["ID"]
-        nodelist.append(aesthetic_id)
+        G.add_node(aesthetic_category["ID"],name=aesthetic_category["Name"])
         for similar_aesthetic in aesthetic_category["Similar_Aesthetics"]:
-            edgelist.append([aesthetic_id, similar_aesthetic])
+            G.add_edge(aesthetic_category["ID"],similar_aesthetic)
 
-#construct the graph
-G.add_nodes_from(nodelist)
-G.add_edges_from(edgelist)
+#store the ID of isolates, then remove from the graph
+isolates = list(nx.isolates(G))
+G.remove_nodes_from(list(nx.isolates(G)))
 
-#removing node 58 because it's an isolate (unconnected to all other nodes), so it compromises the analysis
-G.remove_node(58)
-
-#draw the graph
-nx.draw_spring(G, with_labels=True)
-
-#calculate the diameter of the graph, ie the smallest path between opposite ends of the network
+# #calculate the diameter of the graph, ie the smallest path between opposite ends of the network
 nx.diameter(G)
 
-#calculate the density of the graph, ie # of actual connections divided by # of total possible connections
+# #calculate the density of the graph, ie # of actual connections divided by # of total possible connections
 nx.density(G)
 
-#return a list of bridges, ie edges whose removal would disconnect the graph
+# #return a list of bridges, ie edges whose removal would disconnect the graph
 list(nx.bridges(G))
 
-#return the degree centrality for each node, ie how many edges a node has
+# #return the degree centrality for each node, ie how many edges a node has
 nx.degree_centrality(G)
